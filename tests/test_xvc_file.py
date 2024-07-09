1aafadf96522cf89077334a20f4fbf2b93856427
Emre Sahin <github@emresult.com>
2024-07-09 10:53:22 +0300

    poor man's focus

--- tests/test_xvc_file.py
+++ tests/test_xvc_file.py
@@ -8,48 +8,50 @@ def test_file_track_symlink(xvc_repo_with_dir):
     assert os.path.islink("dir-0001/file-0001")
 
 
-def test_file_hash(xvc_repo_with_dir):
-    assert False
-
-
-def test_file_recheck(xvc_repo_with_dir):
-    assert False
-
-
-def test_file_carry_in(xvc_repo_with_dir):
-    assert False
-
-
-def test_file_copy(xvc_repo_with_dir):
-    assert False
-
-
-def test_file_move(xvc_repo_with_dir):
-    assert False
-
-
-def test_file_list(xvc_repo_with_dir):
-    assert False
-
-
-def test_file_send(xvc_repo_with_dir):
-    assert False
-
-
-def test_file_bring(xvc_repo_with_dir):
-    assert False
-
-
-def test_file_remove(xvc_repo_with_dir):
-    assert False
-
-
-def test_file_untrack(xvc_repo_with_dir):
-    assert False
-
-
-def test_file_share(xvc_repo_with_dir):
-    assert False
-
-
 #
+#
+# def test_file_hash(xvc_repo_with_dir):
+#     assert False
+#
+#
+# def test_file_recheck(xvc_repo_with_dir):
+#     assert False
+#
+#
+# def test_file_carry_in(xvc_repo_with_dir):
+#     assert False
+#
+#
+# def test_file_copy(xvc_repo_with_dir):
+#     assert False
+#
+#
+# def test_file_move(xvc_repo_with_dir):
+#     assert False
+#
+#
+# def test_file_list(xvc_repo_with_dir):
+#     assert False
+#
+#
+# def test_file_send(xvc_repo_with_dir):
+#     assert False
+#
+#
+# def test_file_bring(xvc_repo_with_dir):
+#     assert False
+#
+#
+# def test_file_remove(xvc_repo_with_dir):
+#     assert False
+#
+#
+# def test_file_untrack(xvc_repo_with_dir):
+#     assert False
+#
+#
+# def test_file_share(xvc_repo_with_dir):
+#     assert False
+#
+#
+# #
