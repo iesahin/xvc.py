10723aec5188b74af94d4aa7c8ffc2bc07d492cc
Emre Sahin <github@emresult.com>
2024-07-09 10:46:56 +0300

    xvc-repo-with-dir

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,3 +10,11 @@ def empty_xvc_repo(monkeypatch, tmpdir):
     xvc = Xvc()
     xvc.init()
     return xvc
+
+
+@pytest.fixture
+def xvc_repo_with_dir(empty_xvc_repo):
+    os.system(
+        "xvc-test-helper create-directory-tree --directories 3 --files 3 --seed 42"
+    )
+    return empty_xvc_repo
