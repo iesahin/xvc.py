9313c896498317254b92a1741ba8d6dc83420cee
Emre Sahin <github@emresult.com>
2024-07-09 11:30:14 +0300

    copy

--- tests/test_xvc_file.py
+++ tests/test_xvc_file.py
@@ -15,6 +15,15 @@ def test_file_track_symlink(xvc_repo_with_dir):
     assert os.path.islink("dir-0001/file-0001.bin")
 
 
+def test_file_track_copy(xvc_repo_with_dir):
+    print(os.listdir())
+    xvc_repo_with_dir.file().track("dir-0001/file-0002.bin")
+    assert os.path.isfile("dir-0001/file-0001.bin")
+    assert os.path.isfile(
+        ".xvc/b3/643/2c9/9dec9e4a6c208ab78cfb58749ece5090fa8e279e6fd5a8cfd431e053f5/0.bin"
+    )
+
+
 # def test_file_recheck(xvc_repo_with_dir):
 #     assert False
 #
