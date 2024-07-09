d9a7129b2e11793370f81038fc100cdfda642960
Emre Sahin <github@emresult.com>
2024-07-09 18:51:51 +0300

    carry_in test

--- tests/test_xvc_file.py
+++ tests/test_xvc_file.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 # TODO: write pytests for xvc file
 
 
@@ -30,9 +31,16 @@ def test_file_recheck(xvc_repo_with_dir):
     assert os.path.islink("dir-0001/file-0001.bin")
 
 
-#
-# def test_file_carry_in(xvc_repo_with_dir):
-#     assert False
+def test_file_carry_in(xvc_repo_with_dir):
+    xvc_repo_with_dir.file().track("dir-0001/file-0001.bin")
+    os.remove("dir-0001/file-0001.bin")
+    shutil.copy("dir-0001/file-0002.bin", "dir-0001/file-0001.bin")
+    xvc_repo_with_dir.file().carry_in("dir-0001/file-0001.bin")
+    assert os.path.isfile(
+        ".xvc/b3/643/2c9/9dec9e4a6c208ab78cfb58749ece5090fa8e279e6fd5a8cfd431e053f5/0.bin"
+    )
+
+
 #
 #
 # def test_file_copy(xvc_repo_with_dir):
