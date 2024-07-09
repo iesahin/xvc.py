99a63edb70bc647142935dfca483df76c690f962
Emre Sahin <github@emresult.com>
2024-07-09 10:30:25 +0300

    remove

--- tests/test_xvc_init.py
+++ tests/test_xvc_init.py
@@ -1,25 +1,5 @@
-from xvc import Xvc
-import pytest
 import os
-
-
-@pytest.fixture
-def empty_xvc_repo(monkeypatch, tmpdir):
-    monkeypatch.chdir(tmpdir)
-    os.system("git init")
-    xvc = Xvc(verbosity=4)
-    xvc.init()
-    return tmpdir
 
 
 def test_xvc_init(empty_xvc_repo):
     assert os.path.exists(".xvc")
-
-
-# def test_xvc_root(monkeypatch, tmpdir):
-#     monkeypatch.chdir(tmpdir)
-#     os.system("git init")
-#     xvc = Xvc()
-#     xvc.init()
-#     assert os.path.exists(".xvc")
-#     assert str(xvc.root(absolute=True)) == str(tmpdir.realpath())
