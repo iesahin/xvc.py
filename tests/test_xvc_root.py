00f3f3190cf06b600bffa5be572ec56433a01e4e
Emre Sahin <github@emresult.com>
2024-07-09 10:47:48 +0300

    clean up

--- tests/test_xvc_root.py
+++ tests/test_xvc_root.py
@@ -1,16 +1,4 @@
-# import xvc
-
-# import pytest
 import os
-
-
-# @pytest.fixture
-# def empty_xvc_repo(monkeypatch, tmpdir):
-#     monkeypatch.chdir(tmpdir)
-#     os.system("git init")
-#     xvc_repo = xvc.Xvc()
-#     xvc_repo.init()
-#     return xvc_repo
 
 
 def test_xvc_root(empty_xvc_repo):
