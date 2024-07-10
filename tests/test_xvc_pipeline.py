ceebb39b11d9abde1cde982a62f9b0aff1fa0bce
Emre Sahin <github@emresult.com>
2024-07-10 11:23:54 +0300

    pipeline update

--- tests/test_xvc_pipeline.py
+++ tests/test_xvc_pipeline.py
@@ -26,9 +26,23 @@ def test_pipeline_new(empty_xvc_repo):
     assert pipeline_table == expected
 
 
-# def test_pipeline_update(xvc_repo_with_dir):
-#     assert False
-#
+def test_pipeline_update(empty_xvc_repo)
+    empty_xvc_repo.pipeline().new(pipeline_name="test")
+    empty_xvc_repo.pipeline().update(pipeline_name="test", workdir="dir-0001")
+    pipeline_table = empty_xvc_repo.pipeline().list()
+    print(pipeline_table)
+    expected = """
++---------+----------+
+| Name    | Run Dir  |
++====================+
+| default |          |
+|---------+----------|
+| test    | dir-0001 |
++---------+----------+
+""".strip()
+    assert pipeline_table == expected
+
+
 #
 # def test_pipeline_delete(xvc_repo_with_dir):
 #     assert False
