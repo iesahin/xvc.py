6338143c803a0b1026d38db3fd3658f9af2f59db
Emre Sahin <github@emresult.com>
2024-07-10 10:33:11 +0300

    pipeline_list

--- tests/test_xvc_pipeline.py
+++ tests/test_xvc_pipeline.py
@@ -1,34 +1,38 @@
-def test_pipeline_new(xvc_repo_with_dir):
+def test_pipeline_list(empty_xvc_repo):
+    print(empty_xvc_repo.pipeline().list())
     assert False
 
 
-def test_pipeline_update(xvc_repo_with_dir):
-    assert False
-
-
-def test_pipeline_delete(xvc_repo_with_dir):
-    assert False
-
-
-def test_pipeline_run(xvc_repo_with_dir):
-    assert False
-
-
-def test_pipeline_list(xvc_repo_with_dir):
-    assert False
-
-
-def test_pipeline_dag(xvc_repo_with_dir):
-    assert False
-
-
-def test_pipeline_export(xvc_repo_with_dir):
-    assert False
-
-
-def test_pipeline_import(xvc_repo_with_dir):
-    assert False
-
-
-def test_pipeline_step(xvc_repo_with_dir):
-    assert False
+# def test_pipeline_new(empty_xvc_repo):
+#     assert False
+#
+# def test_pipeline_update(xvc_repo_with_dir):
+#     assert False
+#
+#
+# def test_pipeline_delete(xvc_repo_with_dir):
+#     assert False
+#
+#
+# def test_pipeline_run(xvc_repo_with_dir):
+#     assert False
+#
+#
+# def test_pipeline_list(xvc_repo_with_dir):
+#     assert False
+#
+#
+# def test_pipeline_dag(xvc_repo_with_dir):
+#     assert False
+#
+#
+# def test_pipeline_export(xvc_repo_with_dir):
+#     assert False
+#
+#
+# def test_pipeline_import(xvc_repo_with_dir):
+#     assert False
+#
+#
+# def test_pipeline_step(xvc_repo_with_dir):
+#     assert False
