ed2b56723f5a1622ff69b71b573ab5679e8873a9
Emre Sahin <github@emresult.com>
2024-07-11 12:28:13 +0300

    debug

--- tests/conftest.py
+++ tests/conftest.py
@@ -20,7 +20,7 @@ def xvc_repo_with_dir(empty_xvc_repo):
     return empty_xvc_repo
 
 
-@pytest.fixture
-def xvc_pipeline_single_step(empty_xvc_repo):
-    empty_xvc_repo.pipeline().step().new(step_name="hello", command="echo 'hello xvc'")
-    return empty_xvc_repo
+# @pytest.fixture
+# def xvc_pipeline_single_step(empty_xvc_repo):
+#     empty_xvc_repo.pipeline().step().new(step_name="hello", command="echo 'hello xvc'")
+#     return empty_xvc_repo
