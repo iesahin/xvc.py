189847ef90d446b69bdb7085c8c0422641b9721e
Emre Sahin <github@emresult.com>
2024-07-14 21:46:59 +0300

    update

--- tests/test_xvc_pipeline.py
+++ tests/test_xvc_pipeline.py
@@ -210,11 +210,11 @@ def test_pipeline_step_dependency_regex_items(xvc_repo_with_people_csv):
     )
     pipeline.step().dependency(step_name="a", regex_items="people.csv:/^A.*$")
     pipeline.step().dependency(step_name="b", regex_items="people.csv:/^B.*$")
+    first_run = pipeline.run()
+    print(first_run)
     with open("people.csv", "a") as f:
         f.write("Ali,M,13,74,170\n")
 
-    first_run = pipeline.run()
-    print(first_run)
     second_run = pipeline.run()
     print(second_run)
 
