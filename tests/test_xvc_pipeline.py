ad25e9bdceb5deed763eb4e1af6b20e3190f4f8b
Emre Sahin <github@emresult.com>
2024-07-29 22:05:25 +0300

    params test

--- tests/test_xvc_pipeline.py
+++ tests/test_xvc_pipeline.py
@@ -285,7 +285,7 @@ numeric_param: 13
         )
         pipeline.step().dependency(
             step_name="read-database-config",
-            param=f"{filename}::param",
+            param=f"{filename}::numeric_param",
         )
         first_run = pipeline.run()
         print(first_run)
